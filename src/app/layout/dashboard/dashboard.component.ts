a983ae4745ae104a90f6dbe1c99ec1928f618665
MAIOLIX <stefano.maiola@gmail.com>
2019-09-09 17:57:47 +0200

    Iniziale

---
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -0,0 +1,123 @@
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableDataSource, MatPaginator} from '@angular/material';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+
+
+export interface PeriodicElement {
+    name: string;
+    position: number;
+    weight: number;
+    symbol: string;
+}
+export interface EmoAudioFile {
+    nome: string;
+    url: string;
+    audioData: string;
+    action: string;
+}
+export interface RepoData {
+    nome: string;
+    uri: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' }
+];
+
+const ELEMENT_DATA2: EmoAudioFile[] = [
+    {nome: 'Cliente1', url: 'gs:Cliente1.wav', audioData: 'http://emomaiolix.appspot.com/emotions/repository?file=cliente1.wav', action: 'ANALIZZA'},
+    {nome: 'Cliente1', url: 'gs:Cliente1.wav', audioData: 'http://emomaiolix.appspot.com/emotions/repository?file=cliente1.wav', action: 'ANALIZZA'},
+    {nome: 'Cliente1', url: 'gs:Cliente1.wav', audioData: 'http://emomaiolix.appspot.com/emotions/repository?file=cliente1.wav', action: 'ANALIZZA'},
+    {nome: 'Cliente1', url: 'gs:Cliente1.wav', audioData: 'http://emomaiolix.appspot.com/emotions/repository?file=cliente1.wav', action: 'ANALIZZA'},
+    {nome: 'Cliente1', url: 'gs:Cliente1.wav', audioData: 'http://emomaiolix.appspot.com/emotions/repository?file=cliente1.wav', action: 'ANALIZZA'},
+    {nome: 'Cliente1', url: 'gs:Cliente1.wav', audioData: 'http://emomaiolix.appspot.com/emotions/repository?file=cliente1.wav', action: 'ANALIZZA'},
+    {nome: 'Twilio', url: 'gs:Cliente1.wav', audioData: 'http://emomaiolix.appspot.com/emotions/repository?file=cliente1.wav', action: 'ANALIZZA'},
+    {nome: 'Twilio2', url: 'gs:Cliente1.wav', audioData: 'http://emomaiolix.appspot.com/emotions/repository?file=cliente1.wav', action: 'ANALIZZA'},
+    {nome: 'Stefano', url: 'gs:Cliente1.wav', audioData: 'http://emomaiolix.appspot.com/emotions/repository?file=cliente1.wav', action: 'ANALIZZA'}
+
+
+];
+
+@Component({
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+    constructor(private _httpClient: HttpClient) {}
+    displayedColumns = ['position', 'name', 'weight', 'symbol'];
+    displayedColumns2 = ['nome', 'posizione', 'audio', 'azione'];
+    dataSource = new MatTableDataSource(ELEMENT_DATA);
+    // dataSource2 = new MatTableDataSource(ELEMENT_DATA2);
+    dataSource2 = new MatTableDataSource();
+    places: Array<any> = [];
+    bucketData: Array<RepoData> = [];
+
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+
+    loadData() {
+        console.log('inizio');
+        const requestUrl = 'http://emomaiolix.appspot.com/emotions/repository';
+
+        // const requestUrl = 'http://emomaiolix.appspot.com/emotions/repository';
+
+        this._httpClient.get<RepoData>(requestUrl).subscribe((res) => {
+            for (const appo of res) {
+                this.bucketData.push(appo);
+                console.log('lo sai che sono sempre stato il linguaggio piu bastardo...'); // console.log(this.bucketData.length);
+            }
+        });
+        console.log('fine');
+
+    }
+
+    applyFilter(filterValue: string) {
+        filterValue = filterValue.trim(); // Remove whitespace
+        filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+        this.dataSource2.filter = filterValue;
+    }
+    datiDalServer(): Observable<RepoData[]> {
+        const requestUrl = 'http://emomaiolix.appspot.com/emotions/repository';
+        return  this._httpClient.get<RepoData[]>(requestUrl);
+    }
+    convertiRepoToFileEmo(r: RepoData) {
+        const result: EmoAudioFile = {};
+        const url = 'http://emomaiolix.appspot.com/emotions/repository';
+        result.nome = r.nome;
+        result.url = r.uri;
+        result.audioData = url + '?file=' + r.nome;
+        result.action = 'ANALIZZA';
+        return result;
+
+
+    }
+    ngOnInit() {
+        this.datiDalServer().subscribe(result => {
+            const dati: EmoAudioFile[] = [];
+            result.forEach(element => {
+                if (element.nome.includes('wav')) {
+                    const appo: EmoAudioFile = this.convertiRepoToFileEmo(element);
+                // console.log(element);
+                // const appo: EmoAudioFile = {};
+                // appo.nome = element.nome;
+                 dati.push(appo);
+                }
+
+            });
+            console.log(dati.length);
+            this.dataSource2.data = dati;
+
+        });
+        this.dataSource2.paginator = this.paginator;
+    }
+}
+
